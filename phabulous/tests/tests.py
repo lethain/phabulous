628a98f4ca8910d26b1741724590b7c29833d72d
Will Larson <lethain@gmail.com>
2014-09-28 07:22:24 -0700

    Add tests for projects.

--- phabulous/tests/tests.py
+++ phabulous/tests/tests.py
@@ -8,10 +8,6 @@ class TestPhabulous(unittest.TestCase):
     def setUp(self):
         "Setup test."
         self.responses = Mock()
-        self.phab = phabulous.Phabulous(self.responses)
-
-    def test_user(self):
-        "Test the user object."
         self.responses.configure_mock(**{
             'user.query.return_value': [{
                 'phid': '1',
@@ -29,8 +25,42 @@ class TestPhabulous(unittest.TestCase):
 
                     },
             },
-
+            'project.query.return_value.data': {
+                'phid-1': {
+                    'phid': 'phid-1',
+                    'id': 'id-1',
+                    'name': 'project 1',
+                    'dateCreated': '1/2/3',
+                    'slugs': 'slugs',
+                    'dateModified': '2/3/4',
+                },
+            },
         })
+        self.phab = phabulous.Phabulous(self.responses)
+
+    def test_project(self):
+        "Test the project object."
+        project = self.phab.project(id="will")
+        projects = self.phab.projects(ids=["will"])
+
+        self.assertEquals(len(projects), 1)
+        self.assertEquals(project.phid, projects[0].phid)
+        self.assertEquals(project.phid, 'phid-1')
+        self.assertEquals(project.id, 'id-1')
+
+        self.assertTrue(project.tasks)
+        self.assertTrue(project.members)
+
+        for task in project.tasks:
+            self.assertEquals(task.title, 'task')
+            self.assertEquals(task.dependencies, [])
+
+        for user in project.members:
+            self.assertEquals(user.name, 'will')
+            self.assertEquals(user.email, 'will@example.org')
+
+    def test_user(self):
+        "Test the user object."
         user = self.phab.user(username="will")
         self.assertEquals(user.name, 'will')
         self.assertEquals(user.email, 'will@example.org')
